011a36f55fb48858e874d08f7b548269dd3993c6
DonovanAlejandro <d.alejandro@my.ccsu.edu>
2025-09-24 18:47:55 -0400

    added two lines to test memory

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     print("This is a test")
     print("garbage")
     print("blah")
-
+    print("huh")
+    print ("Final change this is a test at home")
 if __name__ == "__main__":
     main()
