b3c126a2d756f1fdc63e45ef28c04dd27587e429
Erik Jensen <erikalanj@gmail.com>
2025-09-28 21:07:00 -0400

    created a dictionary with key morrighan value sucks

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     print("Hello, GitHub Workshop!")
     print("System.out.println()")
     print("System.out.println()")
-    
+    x = {"morrighan": "sucks"}
 
 
 if __name__ == "__main__":
